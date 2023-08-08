bbba7222dd7cfe2658b7237caf241a39886ac933
guicaroliv <gui.car.dev@gmail.com>
2023-08-08 11:14:20 -0300

    atividade 8

---
+++ fpoo/aula 4/atividades - lista 2/atv7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+Escreva um programa que leia 5 números inteiros em qualquer ordem e 
+exiba na tela ao final, os cinco números em ordem crescente
+*/
+int main(){
+	int a, b, c, d, e;
+	printf("digite 5 numeros inteiros com espaco: ");
+	scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+	return 0;
+}
---
+++ fpoo/aula 4/atividades - lista 2/atv8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+Escreva um programa que leia dois números inteiros e determine qual é o maior e o menor
+*/ 
+int main(){
+	int x, y;
+	printf("digite 2 numeros inteiros com espaco: ");
+	scanf("%d %d",&x,&y);
+	if(x > y)
+		printf("o numero %d e o maior e o %d o menor!",x,y);
+	else
+		printf("o numero %d e o maior e o %d o menor!",y,x);
+	return 0;
+}
