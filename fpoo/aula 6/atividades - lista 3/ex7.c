fe51125a61792987f94602cebab10c18b8b535d0
guicaroliv <gui.car.dev@gmail.com>
2023-08-21 10:29:35 -0300

    aula 6

--- fpoo/aula 6/atividades - lista 3/ex7.c
+++ fpoo/aula 6/atividades - lista 3/ex7.c
@@ -3,10 +3,11 @@
 Faça um programa que imprima na tela a soma dos valores inteiros entre 0 e 100
 */
 int main(){
-	int i;
-	for(i = 1; i < 100; i+=i){
-		i = i + i;
-		printf("%d\n",i);
+	int i, y, x;
+	for(i = 1; i < 99; i++){
+		x = i + i + 1;
+		y = i + 1;
+		printf("%d + %d = %d\n",i,y,x);
 	}
 	return 0;
 }
