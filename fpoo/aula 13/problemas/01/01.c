2b7fd1127274c1210195c57e45631ba68c1b79b4
guicaroliv <gui.car.dev@gmail.com>
2023-09-27 15:05:08 -0300

    27/09/23 - push 2

--- fpoo/aula 13/problemas/01/01.c
+++ fpoo/aula 13/problemas/01/01.c
@@ -4,17 +4,25 @@
 int main(){
 	setlocale(LC_ALL,"");
 	FILE *entrada, *saida;
-	int i = 1, portas[100], multiplo, posicao[100];
+	char portas;
+	int i, multiplo, posicao[100];
 	
 	entrada = fopen("postas.in.txt", "r");
 	if(entrada == NULL){
 		printf("arquivo não encontrado!");
 		return 1;
 	}
-	
+//	printf("%d", portas);
 	saida = fopen("portas.out.txt", "w");
+	for(i = 0; i <= 100; i++){
+		if(fgets(portas, sizeof(portas), entrada) != NULL){
+			atoi(portas);
+			portas = 0;
+			printf("%d", portas);
+		}
+	}
 	
-	
+	fclose(saida);
 	fclose(entrada);
 	
 //	for(i = 1; i <= 100; i++){
