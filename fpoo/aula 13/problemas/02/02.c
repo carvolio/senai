bfa53d170c09e20379581048b64479bc1ac1fbba
matheuspignata <pignatamatheus@gmail.com>
2023-09-28 11:23:24 -0300

    28/09/23 - push 1

---
+++ fpoo/aula 13/problemas/02/02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+
+void posicao(){
+	int total, i;
+	char direcao[1][total];
+	printf("número de comandos: ", total);
+	scanf("%d", &total);
+	for(i = 0; i <= total - 1; i++){
+		printf("direçoes do comando: ");
+		scanf("%s", &direcao[i]);
+	}
+	if()
+	
+}
+
+int main(){
+	setlocale(LC_ALL,"");
+	posicao();
+	return 0;
+}
